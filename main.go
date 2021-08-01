3108ac1e3936fbce30c14d9caeab77829cf1f6bb
Igor Merkushev <merkushevio@yandex.ru>
2021-08-01 23:20:38 +0200

    cors headers allow

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 	exercise.NewSubRouter(router)
 	program.NewSubRouter(router)
 	result.NewSubRouter(router)
+
 	router.PathPrefix("/.well-known/acme-challenge/").Handler(http.FileServer(http.Dir("./certbot/")))
 
 	db := exercise.GetDB(config.DataConnectionConfig, config.App)
@@ -30,7 +31,19 @@ func main() {
 
 	// these two lines are important in order to allow access from the front-end side to the methods
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
-	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT", "OPTIONS"})
+	allowedMethods := handlers.AllowedMethods([]string{
+		http.MethodGet,
+		http.MethodPost,
+		http.MethodPut,
+		http.MethodPatch,
+		http.MethodDelete,
+		http.MethodOptions,
+		http.MethodHead,
+	})
+
+	allowedHeaders := handlers.AllowedHeaders([]string{
+		"*",
+	})
 
 	// Run vk tasks
 	go tasks.CollectVkMessages()
@@ -38,5 +51,5 @@ func main() {
 	// launch server with CORS validations
 	log.Print("Starting server on port :8080")
 	log.Fatal(http.ListenAndServe(":8080",
-		handlers.CORS(allowedOrigins, allowedMethods)(router)))
+		handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(router)))
 }
